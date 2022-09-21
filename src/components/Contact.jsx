67ad540852f7f224f95a99a0b91f689f25d6dd86
Taku10 <tvmadziwa5@gmail.com>
2022-09-21 10:01:26 +0200

    update contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,19 +5,19 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='contact-container'>
-        <div className='contact-heading'>
-            <h1 className='main-heading'>KEEP IN TOUCH</h1>
-            <p className='sub-header'>Setibulum rutrum quam vitae fringilla tincidunt</p>
-            <hr className='purple-line'/>
+      <div className='contact-heading'>
+        <h1 className='main-heading'>KEEP IN TOUCH</h1>
+        <p className='sub-header'>Setibulum rutrum quam vitae fringilla tincidunt</p>
+        <hr className='purple-line' />
+      </div>
+      <form className='contact-form'>
+        <div className='full-name'>
+          <input type="text" name="firstName" placeholder='Enter your First Name' />
+          <input type="text" name="lastName" placeholder='Enter your Last Name' />
         </div>
-        <form className='contact-form'>
-                <div className='full-name'>
-                    <input type="text" name="firstName" placeholder='Enter your First Name' />
-                    <input type="text" name="lastName" placeholder='Enter your Last Name' />
-                </div>
-                <input type="text" name="email" placeholder='Enter your email address'  />
-                <textarea name="message"  cols="10" rows="2"></textarea>
-            </form>
+        <input type="text" name="email" placeholder='Enter your email address' />
+        <textarea name="message" cols="10" rows="10" placeholder='Your Message'></textarea>
+      </form>
     </div>
   )
 }
